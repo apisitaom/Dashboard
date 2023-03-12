b8d7d394cba8b332a10264666d407bdc11c555f0
apisitprompha <apisitprompha@gmail.com>
2023-03-12 09:19:55 +0100

    ADD | STEP 1 add text | Home

--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SkeltonLoading from '../../components/Form/MaterialUI/SkeltonLoading';
 import { 
     Grid, 
@@ -10,22 +10,41 @@ import {
     Tab,
     Container
 } from '@material-ui/core';
+import { Link, Redirect } from 'react-router-dom';
 
 export default function Home(props) {
     const [loading, SetLoading] = useState(false);
-
     return (
         loading ? (<SkeltonLoading />)
         :
         <React.Fragment>
             <>
-                <Grid container spacing={0}>
-                    <Grid item xs={12}>
-                        <Container>
-                            <Typography> Viper Dashboard</Typography>
-                        </Container>
-                    </Grid>
+            <Grid container spacing={0}>
+            {/* Header */}
+                <Grid item xs={11}>
+                    <Container style={{ justifyContent: "center", display: "flex", paddingTop: "1%" }}>
+                        <Typography style={{ paddingLeft: "7%" }} variant="subtitle1" display="block" gutterBottom> News </Typography>
+                        <Typography style={{ paddingLeft: "2.5%" }} variant="subtitle1" display="block" gutterBottom> Cryptocurrency's</Typography>
+                        <Typography style={{ paddingLeft: "2.5%" }} variant="subtitle1" display="block" gutterBottom> Effectation </Typography>
+                    </Container>
                 </Grid>
+                
+                <Grid style={{ paddingTop: "1%" }} item xs={1}> 
+                    <Button style={{ borderRadius: 20 }} variant="outlined">Sign In</Button>
+                </Grid>
+            </Grid>
+            {/* Content */}
+            <Grid item xs={12}>
+                <Container style={{ justifyContent: "center", display: "flex", paddingTop: "3%" }}>
+                    <Typography variant="h3" display="block"> Take Control Profolio </Typography>
+                </Container>
+                <Container style={{ justifyContent: "center", display: "flex", paddingTop: "1%" }}>
+                    <Typography variant="h3" display="block"> Statistic Viper Dashboard page </Typography>
+                </Container>
+                <Container style={{ justifyContent: "center", display: "flex", paddingTop: "3%" }}>
+                    <Button color='primary' style={{ borderRadius: 20 }} variant="contained">Your Analysis / Analysis</Button>
+                </Container>
+            </Grid>
             </>
         </React.Fragment>
     )
