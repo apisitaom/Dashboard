813624746a2ee627c534423070722f512455bf85
apisitprompha <apisitprompha@email.com>
2023-10-01 09:24:13 +0100

    EDIT | mvoe iamge footer | FINISH image footer

--- src/view/Home/Home.js
+++ src/view/Home/Home.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import SkeltonLoading from '../../components/Form/MaterialUI/SkeltonLoading';
 import { Link, Redirect } from 'react-router-dom';
-import { 
-    Grid, 
-    Button, 
+import {
+    Grid,
+    Button,
     Typography,
     Tabs,
     Tab,
@@ -14,152 +14,174 @@ import {
     CardActions,
 } from '@material-ui/core';
 import { i18n } from '../../i18n/index';
+import animeForestImg from '../../images/animeForest.PNG';
+import animeKongVSGodImg from '../../images/animeKongVSGod.PNG';
+import animeTownImg from '../../images/animeTown.PNG';
 
 export default function Home(props) {
     const [loading, SetLoading] = useState(false);
+    const windowWidth = useRef(window.innerWidth);
+    const windowHeight = useRef(window.innerHeight);
+    console.log('windowWidth', windowWidth)
+    console.log('windowHeight', windowHeight)
+
+    const widthFooterImg = windowWidth / 4;
+    const heightFooterImg = windowHeight / 2.5;
+
     return (
         loading ? (<SkeltonLoading />)
-        :
-        <React.Fragment>
-            <>
-            <Grid container spacing={0}>
-            {/* Header */}
-                <Grid item xs={11}>
-                    <Container style={{ justifyContent: "center", display: "flex", paddingTop: "1%" }}>
-                        <Typography style={{ paddingLeft: "7%" }} variant="subtitle1" display="block" gutterBottom> News </Typography>
-                        <Typography style={{ paddingLeft: "2.5%" }} variant="subtitle1" display="block" gutterBottom> Cryptocurrency's</Typography>
-                        <Typography style={{ paddingLeft: "2.5%" }} variant="subtitle1" display="block" gutterBottom> Effectation </Typography>
-                    </Container>
-                </Grid>
-                
-                <Grid style={{ paddingTop: "1%" }} item xs={1}> 
-                    <Button style={{ borderRadius: 20 }} variant="outlined">Sign In</Button>
-                </Grid>
-            </Grid>
-            {/* Content */}
-            <Grid item xs={12}>
-                <Container style={{ justifyContent: "center", display: "flex", paddingTop: "3%" }}>
-                    <Typography variant="h3" display="block"> Take Control Your Port </Typography>
-                </Container>
-                <Container style={{ justifyContent: "center", display: "flex", paddingTop: "1%" }}>
-                    <Typography variant="h3" display="block"> Statistic Viper Dashboard Page </Typography>
-                </Container>
-                <Container style={{ justifyContent: "center", display: "flex", paddingTop: "3%" }}>
-                    <Button color='primary' style={{ borderRadius: 20 }} variant="contained">Your Analysis / Analysis</Button>
-                </Container>
-            </Grid>
-            {/* Chart */}
-            <Grid style={{ paddingTop: "2.5%" }} item xs={12}>
-                <Container style={{ justifyContent: "center", display: "flex", paddingTop: "1%" }}>
-                    {/* Chat 1 */}
-                    <Grid item xs={4}>
+            :
+            <React.Fragment>
+                <>
+                    <Grid container spacing={0}>
+                        {/* Header */}
+                        <Grid item xs={11}>
+                            <Container style={{ justifyContent: "center", display: "flex", paddingTop: "1%" }}>
+                                <Typography style={{ paddingLeft: "7%" }} variant="subtitle1" display="block" gutterBottom> News </Typography>
+                                <Typography style={{ paddingLeft: "2.5%" }} variant="subtitle1" display="block" gutterBottom> Cryptocurrency's</Typography>
+                                <Typography style={{ paddingLeft: "2.5%" }} variant="subtitle1" display="block" gutterBottom> Effectation </Typography>
+                            </Container>
+                        </Grid>
+
+                        <Grid style={{ paddingTop: "1%" }} item xs={1}>
+                            <Button style={{ borderRadius: 20 }} variant="outlined">Sign In</Button>
+                        </Grid>
+                    </Grid>
+                    {/* Content */}
+                    <Grid item xs={12}>
+                        <Container style={{ justifyContent: "center", display: "flex", paddingTop: "3%" }}>
+                            <Typography variant="h3" display="block"> Take Control Your Port </Typography>
+                        </Container>
                         <Container style={{ justifyContent: "center", display: "flex", paddingTop: "1%" }}>
-                            <Typography style={{ paddingLeft: "7%" }} variant="subtitle1" display="block" gutterBottom> Chart </Typography>
-                    </Container>
-                    </Grid>                    
-                    {/* Chat 2 */}
-                    <Grid item xs={4}>
-                        <Container style={{ justifyContent: "center", display: "flex", paddingTop: "1%" }}>
-                            <Typography style={{ paddingLeft: "7%" }} variant="subtitle1" display="block" gutterBottom> Chart Statistic </Typography>
+                            <Typography variant="h3" display="block"> Statistic Viper Dashboard Page </Typography>
+                        </Container>
+                        <Container style={{ justifyContent: "center", display: "flex", paddingTop: "3%" }}>
+                            <Button color='primary' style={{ borderRadius: 20 }} variant="contained">Your Analysis / Analysis</Button>
                         </Container>
                     </Grid>
-                    {/* Chat 3 */}
-                    <Grid item xs={4}>
-                        <Container style={{ justifyContent: "center", paddingTop: "1%" }}>
-                        <Card style={{ width: "80%", backgroundColor: "#edf3f2c7" }}>
-                            <CardContent>
-                                <Typography variant="h6" display="block" gutterBottom>
-                                  {5235} $
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                        <Card style={{ width: "80%", marginTop: "2%", backgroundColor: "#edf3f2c7"  }}>
-                            <CardContent>
-                                <Typography variant="h6" display="block" gutterBottom>
-                                  5.235
-                                </Typography>
-                            </CardContent>
-                        </Card>
+                    {/* Chart */}
+                    <Grid style={{ paddingTop: "2.5%" }} item xs={12}>
+                        <Container style={{ justifyContent: "center", display: "flex", paddingTop: "1%" }}>
+                            {/* Chat 1 */}
+                            <Grid item xs={4}>
+                                <Container style={{ justifyContent: "center", display: "flex", paddingTop: "1%" }}>
+                                    <Typography style={{ paddingLeft: "7%" }} variant="subtitle1" display="block" gutterBottom> Chart </Typography>
+                                </Container>
+                            </Grid>
+                            {/* Chat 2 */}
+                            <Grid item xs={4}>
+                                <Container style={{ justifyContent: "center", display: "flex", paddingTop: "1%" }}>
+                                    <Typography style={{ paddingLeft: "7%" }} variant="subtitle1" display="block" gutterBottom> Chart Statistic </Typography>
+                                </Container>
+                            </Grid>
+                            {/* Chat 3 */}
+                            <Grid item xs={4}>
+                                <Container style={{ justifyContent: "center", paddingTop: "1%" }}>
+                                    <Card style={{ width: "80%", backgroundColor: "#edf3f2c7" }}>
+                                        <CardContent>
+                                            <Typography variant="h6" display="block" gutterBottom>
+                                                {5235} $
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                    <Card style={{ width: "80%", marginTop: "2%", backgroundColor: "#edf3f2c7" }}>
+                                        <CardContent>
+                                            <Typography variant="h6" display="block" gutterBottom>
+                                                5.235
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                </Container>
+                            </Grid>
                         </Container>
+                        {/* Detail text */}
+                        <Grid item xs={12}>
+                            <Container style={{ justifyContent: "center", display: "flex", paddingTop: "3%" }}>
+                                <Typography variant="body2" display="block" gutterBottom>
+                                    20+ COMPANY HAS SHOWED INTEREST TO OUR NEW VIPER DASHBOARD TOOL
+                                </Typography>
+                            </Container>
+                        </Grid>
+                        {/* Icons Compay to use dashboard */}
+                        <Grid item xs={12}>
+                            <Container style={{ justifyContent: "center", display: "flex", paddingTop: "1%" }}>
+                                <Card style={{ width: "17%", backgroundColor: "#ABB2B9" }}>
+                                    <CardContent>
+                                        <Typography style={{ paddingLeft: "2.5%", textAlign: 'center', color: "#17202A" }} variant="subtitle1" display="block" gutterBottom>
+                                            Slack
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                                <Card style={{ marginLeft: "5%", width: "17%", backgroundColor: "#ABB2B9" }}>
+                                    <CardContent>
+                                        <Typography style={{ paddingLeft: "2.5%", textAlign: 'center', color: "#17202A" }} variant="subtitle1" display="block" gutterBottom>
+                                            Quicken Leans
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                                <Card style={{ marginLeft: "5%", width: "17%", backgroundColor: "#ABB2B9" }}>
+                                    <CardContent>
+                                        <Typography style={{ paddingLeft: "2.5%", textAlign: 'center', color: "#17202A" }} variant="subtitle1" display="block" gutterBottom>
+                                            Disney
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                                <Card style={{ marginLeft: "5%", width: "17%", backgroundColor: "#ABB2B9" }}>
+                                    <CardContent>
+                                        <Typography style={{ paddingLeft: "2.5%", textAlign: 'center', color: "#17202A" }} variant="subtitle1" display="block" gutterBottom>
+                                            :hager
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                                <Card style={{ marginLeft: "5%", width: "17%", backgroundColor: "#ABB2B9" }}>
+                                    <CardContent style={{ textAlign: 'center', color: "#17202A" }}>
+                                        <Typography style={{ paddingLeft: "2.5%" }} variant="subtitle1" display="block" gutterBottom>
+                                            NETFLIX
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Container>
+                        </Grid>
+                        {/* Detail text next feature */}
+                        <Grid item xs={12}>
+                            <Container style={{ justifyContent: "center", display: "flex", paddingTop: "6%" }}>
+                                <Typography variant="h4" display="block" gutterBottom>
+                                    FEATURE TO GET STARTED WITH
+                                </Typography>
+                            </Container>
+                            <Container style={{ justifyContent: "center", display: "flex" }}>
+                                <Typography variant="caption" display="block" gutterBottom>
+                                    Best your management for free try, keep grow up your port with viper dashboard
+                                </Typography>
+                            </Container>
+                        </Grid>
+                        {/* Icon like award */}
+                        <Grid item xs={12}>
+                            <Container style={{ justifyContent: "center", display: "flex", paddingTop: "1%" }}>
+                                <Grid xs={4}>
+                                    <div style={{ width: 100, height: 100 }}>
+                                        <img
+                                            style={{ width: widthFooterImg, height: heightFooterImg, margin: "1.5%", borderRadius: 15 }}
+                                            src={animeForestImg}
+                                            alt='animeForest' />
+                                    </div>
+                                </Grid>
+                                <Grid xs={4}>
+                                    <img
+                                        style={{ width: widthFooterImg, height: heightFooterImg, margin: "1.5%", borderRadius: 15 }}
+                                        src={animeKongVSGodImg}
+                                        alt='animeKongVSGod' />
+                                </Grid>
+                                <Grid xs={4}>
+                                    <img
+                                        style={{ width: widthFooterImg, height: heightFooterImg, margin: "1.5%", borderRadius: 15 }}
+                                        src={animeTownImg}
+                                        alt='animeTown' />
+                                </Grid>
+                            </Container>
+                        </Grid>
                     </Grid>
-                </Container>
-                {/* Detail text */}
-                <Grid item xs={12}>
-                    <Container style={{ justifyContent: "center", display: "flex", paddingTop: "3%" }}>
-                        <Typography variant="body2" display="block" gutterBottom>
-                          20+ COMPANY HAS SHOWED INTEREST TO OUR NEW VIPER DASHBOARD TOOL
-                        </Typography>
-                    </Container>
-                </Grid>
-                {/* Icons Compay to use dashboard */}
-                <Grid item xs={12}>
-                    <Container style={{ justifyContent: "center", display: "flex", paddingTop: "1%" }}>
-                        <Card style={{ width: "17%", backgroundColor: "#ABB2B9" }}>
-                            <CardContent>
-                                <Typography style={{ paddingLeft: "2.5%", textAlign: 'center', color: "#17202A" }} variant="subtitle1" display="block" gutterBottom> 
-                                    Slack
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                        <Card style={{ marginLeft: "5%", width: "17%", backgroundColor: "#ABB2B9" }}>
-                            <CardContent>
-                                <Typography style={{ paddingLeft: "2.5%", textAlign: 'center', color: "#17202A" }} variant="subtitle1" display="block" gutterBottom> 
-                                    Quicken Leans
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                        <Card style={{ marginLeft: "5%", width: "17%", backgroundColor: "#ABB2B9" }}>
-                            <CardContent>
-                                <Typography style={{ paddingLeft: "2.5%", textAlign: 'center', color: "#17202A" }} variant="subtitle1" display="block" gutterBottom> 
-                                    Disney
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                        <Card style={{ marginLeft: "5%", width: "17%", backgroundColor: "#ABB2B9" }}>
-                            <CardContent>
-                                <Typography style={{ paddingLeft: "2.5%", textAlign: 'center', color: "#17202A" }} variant="subtitle1" display="block" gutterBottom> 
-                                    :hager
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                        <Card style={{ marginLeft: "5%", width: "17%", backgroundColor: "#ABB2B9" }}>
-                            <CardContent style={{ textAlign: 'center', color: "#17202A" }}>
-                                <Typography style={{ paddingLeft: "2.5%" }} variant="subtitle1" display="block" gutterBottom> 
-                                    NETFLIX
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Container>
-                </Grid>
-                {/* Detail text next feature */}
-                <Grid item xs={12}>
-                    <Container style={{ justifyContent: "center", display: "flex", paddingTop: "6%" }}>
-                        <Typography variant="h4" display="block" gutterBottom>
-                            FEATURE TO GET STARTED WITH
-                        </Typography>
-                    </Container>
-                    <Container style={{ justifyContent: "center", display: "flex"}}>
-                        <Typography variant="caption" display="block" gutterBottom>
-                           Best your management for free try, keep grow up your port with viper dashboard
-                        </Typography>
-                    </Container>
-                </Grid>
-                {/* Icon like award */}
-                <Grid item xs={12}>
-                    <Container style={{ justifyContent: "center", display: "flex", paddingTop: "1%" }}>
-                        <Grid xs={4}>
-                            <p>Icon + text 1</p>
-                        </Grid>
-                        <Grid xs={4}>
-                            <p>Icon + text 2</p>
-                        </Grid>
-                        <Grid xs={4}>
-                            <p>Icon + text 3</p>
-                        </Grid>
-                    </Container>
-                </Grid>
-            </Grid>
-            </>
-        </React.Fragment>
+                </>
+            </React.Fragment>
     )
 }
